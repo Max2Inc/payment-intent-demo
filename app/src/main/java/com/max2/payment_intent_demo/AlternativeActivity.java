40f71317e3e7ff4d1e8c580a8ef705d263912674
oradkovsky <ostap.radkovskyy@gmail.com>
2018-03-15 18:26:22 +0200

    sdk based login integration example

---
+++ app/src/main/java/com/max2/payment_intent_demo/AlternativeActivity.java
@@ -0,0 +1,25 @@
+package com.max2.payment_intent_demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.max2.veeaconnect.sdk.ui.authentication.AuthActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AlternativeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alternative);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bntLogin)
+    protected void onButtonLoginClick() {
+        startActivity(new Intent(this, AuthActivity.class));
+    }
+}
--- app/src/main/java/com/max2/payment_intent_demo/App.java
+++ app/src/main/java/com/max2/payment_intent_demo/App.java
@@ -3,7 +3,11 @@ package com.max2.payment_intent_demo;
 import android.app.Application;
 
 import com.google.gson.Gson;
+import com.max2.veeaconnect.sdk.Max2SdkInternal;
 import com.squareup.otto.Bus;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class App extends Application {
     protected static App me;
@@ -16,6 +20,20 @@ public class App extends Application {
         super.onCreate();
 
         me = this;
+
+        initMax2Sdk();
+    }
+
+    protected void initMax2Sdk() {
+        Max2SdkInternal.builder(this)
+                .subscribeOnScheduler(Schedulers.io())
+                .observeOnScheduler(AndroidSchedulers.mainThread())
+                .baseUrl(BuildConfig.ENDPOINT_URL) //provided by Max2
+                .debug(BuildConfig.DEBUG)
+                .kiccIpAddress(BuildConfig.KICC_IP_ADDRESS) //provided by Max2
+                .kiccPort(BuildConfig.KICC_TRANSACTION_PORT) //provided by Max2
+                .dummyPayments(BuildConfig.DUMMY_PAYMENTS)
+                .init();
     }
 
     public static App getInstance() {
